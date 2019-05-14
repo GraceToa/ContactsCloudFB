9d92a8931ee01ab4d8c8f2b0b96bbab32bd23d6d
GraceToa <gracetoa@uoc.edu>
2019-05-14 16:39:39 +0200

    demo contacts

---
+++ CrudCloudFirestore/User.swift
@@ -0,0 +1,9 @@
+//
+//  User.swift
+//  CrudCloudFirestore
+//
+//  Created by GraceToa on 06/05/2019.
+//  Copyright © 2019 GraceToa. All rights reserved.
+//
+
+import Foundation
---
+++ CrudCloudFirestore/UserTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  UserTableViewCell.swift
+//  CrudCloudFirestore
+//
+//  Created by GraceToa on 07/05/2019.
+//  Copyright © 2019 GraceToa. All rights reserved.
+//
+
+import UIKit
+
+class UserTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
